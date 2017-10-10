de2f4e51407d54f2a833316d1452b9b2a0985716
anorganix <anorganix@gmail.com>
2017-10-10 23:25:47 +0300

    Removed old / invalid version (kernel error)

--- algorithm/phi1612.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHI1612_H
-#define PHI1612_H
-
-#include "miner.h"
-
-void precalc_hash_phi1612(dev_blk_ctx *blk, uint32_t *midstate, uint32_t *pdata);
-void phi1612_regenhash(struct work *work);
-
-#endif
